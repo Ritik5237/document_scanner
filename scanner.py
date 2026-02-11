39ee48c89e74d9579df005008c9c259aa8eb5057
ritikbhardwaj <ritikbhardwaj05@gmail.com>
2026-02-11 21:34:12 +0530

    Task 1: Project setup and welcome system

---
+++ scanner.py
@@ -0,0 +1,26 @@
+# =====================================================
+# Name: Ritik Sharma
+# Roll No: 2301010342
+# Course: Image Processing & Computer Vision
+# Unit: Image Acquisition & Enhancement
+# Assignment: Smart Document Scanner System
+# Date: 09-Feb-2026
+# =====================================================
+
+
+def welcome():
+    print("="*60)
+    print(" SMART DOCUMENT SCANNER SYSTEM ")
+    print("="*60)
+    print("Analyzing Sampling & Quantization Effects")
+    print()
+
+
+def main():
+    welcome()
+
+
+if __name__ == "__main__":
+    main()
+
+
